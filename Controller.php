f9acec0e78154ae63109b339b7bf3e7ba81e93a8
Amber Suratte <as00193@my.westga.edu>
2021-04-18 16:40:19 -0500

    Added skeleton of Controller, pointed index.php to it

--- Controller.php
+++ Controller.php
@@ -1,8 +1,86 @@
 <?php
+require_once './model/Database.php';
+require_once './model/Validator.php';
+require_once 'autoload.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+class Controller
+{
+    private $action;
+    private $db;
+    private $twig;
+    
+    /**
+     * Instantiates a new controller
+     */
+    public function __construct() {
+        $loader = new Twig\Loader\FilesystemLoader('./view');
+        $this->twig = new Twig\Environment($loader);
+        $this->setSecureConnectionAndSession();
+        //$this->connectToDatabase();
+        $this->action = $this->getAction();
+    }
+    
+    /**
+     * Initiates processing of the current action
+     */
+    public function invoke() {
+        switch($this->action) {
+            default:
+                $this->processShowHomePage();
+                break;
+        }
+    }
+    
+    /**
+     * Processes request
+     */
+    private function processShowHomePage() {
+        $template = $this->twig->load('home.twig');
+        echo $template->render();
+    }
+    
+    /**
+     * Gets the action from $_GET or $_POST array
+     * 
+     * @return string the action to be processed
+     */
+    private function getAction() {
+        $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        if ($action === NULL) {
+            $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            if ($action === NULL) {
+                $action = '';
+            }
+        }
+        return $action;
+    }
+    
+    /**
+     * Sets up HTTPS connection and session
+     */
+    private function setSecureConnectionAndSession() {
+        $https = filter_input(INPUT_SERVER, 'HTTPS');
+        if (!$https) {
+            $host = filter_input(INPUT_SERVER, 'HTTP_HOST');
+            $uri = filter_input(INPUT_SERVER, 'REQUEST_URI');
+            $url = 'https://' . $host . $uri;
+            header("Location: " . $url);
+            exit();
+        }
+        session_start();
+        $this->twig->addGlobal('session', $_SESSION);
+    }
+    
+    /**
+     * Connects to the database
+     
+    private function connectToDatabase() {
+        $this->db = new Database();
+        if (!$this->db->isConnected()) {
+            $error_message = $this->db->getErrorMessage();
+            $template = $this->twig->load('database_error.twig');
+            echo $template->render(['error_message' => $error_message]);
+            exit();
+        }
+    }*/
+}
